73d9e5ef344697194e43e990278cd26faf7c45d7
michael <mmo@crafttec.ai>
2021-05-26 18:21:07 +0800

    pixel ratio 1.5

--- lib/image_editor_pro.dart
+++ lib/image_editor_pro.dart
@@ -126,7 +126,7 @@ class _ImageEditorProState extends State<ImageEditorPro> {
               onPressed: () {
                 screenshotController
                     .capture(
-                        delay: Duration(milliseconds: 500), pixelRatio: 1)
+                        delay: Duration(milliseconds: 500), pixelRatio: 1.5)
                     .then((binaryIntList) async {
                   //print("Capture Done");
 
